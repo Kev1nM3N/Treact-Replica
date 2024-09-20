f05d78d96f61666313373801e2c1e216cf1a2e7a
Kevin Mendez <mediaman502@aol.com>
2024-09-20 13:31:53 -0400

    Check correct repository

---
+++ treact.js
@@ -0,0 +1,9 @@
+function openMenu (){
+    document.body.classList += (` menu__open`)
+}
+
+function closeMenu (){
+    document.body.classList.remove(`menu__open`)
+}
+
+console.log('Completed on 2023');
